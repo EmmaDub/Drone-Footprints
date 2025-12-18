670607c45a1b6032a65d83ea510fb2dca315e8e6
EmmaDub <102745894+EmmaDub@users.noreply.github.com>
2025-12-17 19:31:45 -0500

    Update new_fov.py

--- src/new_fov.py
+++ src/new_fov.py
@@ -109,9 +109,9 @@ class HighAccuracyFOVCalculator:
         self.image.find_declination()
         declination = self.image.declination
 
-        adj_yaw, adj_pitch, adj_roll = self.calculate_rads_from_angles(self.flight_yaw_degree,
-                                                                       self.flight_pitch_degree,
-                                                                       self.flight_roll_degree,
+        adj_yaw, adj_pitch, adj_roll = self.calculate_rads_from_angles(self.image.flight_yaw_degree,
+                                                                       self.image.flight_pitch_degree,
+                                                                       self.image.flight_roll_degree,
                                                                        declination)
 
         q = quaternion.from_euler_angles(adj_yaw, adj_pitch, adj_roll)
